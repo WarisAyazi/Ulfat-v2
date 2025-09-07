780b76b3a7b5a8238b6bd19a6067c5d6d61227b4
MustafaAmirzada <mustafa.amirzada.afg@gmail.com>
2025-09-07 10:49:47 +0430

    adding requests of the controllers

---
+++ app/Http/Requests/EnrollmentStoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EnrollmentStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'amount' => ['required', 'integer'],
+            'month' => ['required', 'string', 'max:30'],
+            'year' => ['required'],
+            'time' => ['required'],
+            'student_id' => ['required', 'integer', 'exists:students,id'],
+            'course_id' => ['required', 'integer', 'exists:courses,id'],
+        ];
+    }
+}
