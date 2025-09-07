99d01bc0effdcdba6521ca6f23ab4c6af84c8629
MustafaAmirzada <mustafa.amirzada.afg@gmail.com>
2025-09-07 11:08:24 +0430

    adding Controllers

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StudentStoreRequest;
+use App\Http\Requests\StudentUpdateRequest;
+use App\Models\Student;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class StudentController extends Controller
+{
+    public function index(Request $request): Response
+    {
+        $students = Student::all();
+
+        return Inertia::render('Students', [
+            'students' => $students,
+        ]);
+    }
+
+    public function create(Request $request): Response
+    {
+        return Inertia::render('student.create');
+    }
+
+    public function store(StudentStoreRequest $request): RedirectResponse
+    {
+        $student = Student::create($request->validated());
+
+        $request->session()->flash('student.id', $student->id);
+
+        return redirect()->route('students.index');
+    }
+
+    public function show(Request $request, Student $student): Response
+    {
+        return Inertia::render('student.show', [
+            'student' => $student,
+        ]);
+    }
+
+    public function edit(Request $request, Student $student): Response
+    {
+        return Inertia::render('student.edit', [
+            'student' => $student,
+        ]);
+    }
+
+    public function update(StudentUpdateRequest $request, Student $student): RedirectResponse
+    {
+        $student->update($request->validated());
+
+        $request->session()->flash('student.id', $student->id);
+
+        return redirect()->route('students.index');
+    }
+
+    public function destroy(Request $request, Student $student): RedirectResponse
+    {
+        $student->delete();
+
+        return redirect()->route('students.index');
+    }
+}
---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TeacherStoreRequest;
+use App\Http\Requests\TeacherUpdateRequest;
+use App\Models\Teacher;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class TeacherController extends Controller
+{
+    public function index(Request $request): Response
+    {
+        $teachers = Teacher::all();
+
+        return Inertia::render('Teachers', [
+            'teachers' => $teachers,
+        ]);
+    }
+
+    public function create(Request $request): Response
+    {
+        return Inertia::render('teacher.create');
+    }
+
+    public function store(TeacherStoreRequest $request): RedirectResponse
+    {
+        $teacher = Teacher::create($request->validated());
+
+        $request->session()->flash('teacher.id', $teacher->id);
+
+        return redirect()->route('teachers.index');
+    }
+
+    public function show(Request $request, Teacher $teacher): Response
+    {
+        return Inertia::render('teacher.show', [
+            'teacher' => $teacher,
+        ]);
+    }
+
+    public function edit(Request $request, Teacher $teacher): Response
+    {
+        return Inertia::render('teacher.edit', [
+            'teacher' => $teacher,
+        ]);
+    }
+
+    public function update(TeacherUpdateRequest $request, Teacher $teacher): RedirectResponse
+    {
+        $teacher->update($request->validated());
+
+        $request->session()->flash('teacher.id', $teacher->id);
+
+        return redirect()->route('teachers.index');
+    }
+
+    public function destroy(Request $request, Teacher $teacher): RedirectResponse
+    {
+        $teacher->delete();
+
+        return redirect()->route('teachers.index');
+    }
+}
